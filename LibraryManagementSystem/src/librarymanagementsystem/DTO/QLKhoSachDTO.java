b5a5e547a4b598e53e93cc020ec63c796574dc6f
Viet Anh <vietanhtr923@gmail.com>
2024-05-26 23:34:15 +0700

    nccsach

--- LibraryManagementSystem/src/librarymanagementsystem/DTO/QLKhoSachDTO.java
+++ LibraryManagementSystem/src/librarymanagementsystem/DTO/QLKhoSachDTO.java
@@ -1,5 +1,42 @@
 package librarymanagementsystem.DTO;
 
 public class QLKhoSachDTO {
+     String maSach;
+    int soLuong;
+    QLLoaiSachDTO sach; //for full view table (not use in normal view)
 
+    public QLKhoSachDTO(String maSach, int soLuong) {
+        this.maSach = maSach;
+        this.soLuong = soLuong;
+    }
+
+    public QLKhoSachDTO(String maSach, int soLuong, QLLoaiSachDTO sach){
+        this.maSach = maSach;
+        this.soLuong = soLuong;
+        this.sach = new QLLoaiSachDTO(sach);
+    }
+
+    public String getMaSach() {
+        return maSach;
+    }
+
+    public void setMaSach(String maSach) {
+        this.maSach = maSach;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public QLLoaiSachDTO getSach() {
+        return sach;
+    }
+
+    public void setSach(QLLoaiSachDTO sach) {
+        this.sach = sach;
+    }
 }
